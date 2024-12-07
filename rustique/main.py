1f60d2d9b1b4e98d96cd321337117cf744dc4e27
Philip Solarz <philipsolarz@outlook.com>
2024-12-07 21:23:59 +0100

    Add initial project structure and dependencies for Rustique

--- rustique/main.py
+++ rustique/main.py
@@ -1,50 +1,35 @@
-from rustique import List
+# from rustique import List
+import rustique as rs
 
-def test_list():
-    # Test creation of an empty list
-    lst = List()
-    print("Empty list:", lst)  # Expect: []
+def test_num():
+    a = rs.int(1)
+    assert a == rs.int(1)
+    assert a == 1
+    # assert a == 1.0
+    assert 1 == 1.0
+    # assert a < rs.int(2)
+    # assert a <= rs.int(2)
+    # assert a > rs.int(0)
+    # assert a >= rs.int(0)
 
-    # Test creation with initial elements
-    lst2 = List([1, 2, 3])
-    print("List with elements:", lst2)  # Expect: [1, 2, 3]
 
-    # Test appending elements
-    lst2.append(4)
-    print("After appending 4:", lst2)  # Expect: [1, 2, 3, 4]
 
-    # Test sum of elements
-    print("Sum of elements:", lst2.sum())  # Expect: 10
+def test0_eq():
+    a = rs.list()
+    assert a == rs.list()
+    # assert a == []
+    # assert a == rs.list([])
 
-    # Test length
-    print("Length of list:", len(lst2))  # Expect: 4
+    # b = rs.list([1, 2, 3])
+    # assert b == rs.list([1, 2, 3])
+    # assert b == [1, 2, 3]
 
-    # Test indexing (__getitem__)
-    print("Element at index 1:", lst2[1])  # Expect: 2
+    # assert not a == b
 
-    # Test setting an element (__setitem__)
-    lst2[1] = 99
-    print("After setting index 1 to 99:", lst2)  # Expect: [1, 99, 3, 4]
-
-    # Test deleting an element (__delitem__)
-    del lst2[2]
-    print("After deleting index 2:", lst2)  # Expect: [1, 99, 4]
-
-    # Test __contains__
-    print("Contains 99:", 99 in lst2)  # Expect: True
-    print("Contains 3:", 3 in lst2)    # Expect: False
-
-    # Test __str__ and __repr__
-    print("String representation:", str(lst2))  # Expect: [1, 99, 4]
-    print("Repr representation:", repr(lst2))  # Expect: List([1, 99, 4])
-
-    # Test addition (__add__)
-    lst3 = lst2 + List([5, 6])
-    print("After adding [5, 6]:", lst3)  # Expect: [1, 99, 4, 5, 6]
-
-    # Test multiplication (__mul__)
-    lst4 = lst2 * 2
-    print("After multiplying by 2:", lst4)  # Expect: [1, 99, 4, 1, 99, 4]
+def tests():
+    test_num()
+    # test0_eq()
 
 if __name__ == "__main__":
-    test_list()
+    tests()
+    print("Rustique is working!")
--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,386 @@
-use pyo3::prelude::*;
-use pyo3::types::{PyAny, PySequence};
-use pyo3::exceptions::PyIndexError;
+use std::borrow::{Borrow, BorrowMut};
 
+use num_bigint::BigInt;
+use pyo3::basic::CompareOp;
+use pyo3::prelude::*;
+use rug::{Float, Integer};
+use pyo3::types::{
+    PyAny, PyInt, 
+    // PyList, 
+    // PySequence, 
+    // PyString
+};
+// use pyo3::exceptions::PyIndexError;
+// use pyo3::basic::CompareOp; // for CompareOp::Eq
+// use std::collections::hash_map::DefaultHasher;
+// use std::hash::{Hash, Hasher};
+// use rug::Integer;
+// use num_bigint::BigInt;
+
+#[derive(FromPyObject)]
 #[pyclass]
-struct List {
-    elements: Vec<PyObject>
-}
+struct int(BigInt);
 
 #[pymethods]
-impl List {
+impl int {
     #[new]
-    #[pyo3(signature = (elements=None))]
-    fn new(elements: Option<Vec<i32>>) -> Self {
-        List {
-            elements: elements.unwrap_or_default()
-        }
-    }
-
-    fn append(&mut self, element: i32) {
-        self.elements.push(element);
-    }
-
-    fn sum(&self) -> i32 {
-        self.elements.iter().sum()
-    }
-
-    fn __len__(&self) -> usize {
-        self.elements.len()
-    }
-
-    fn __getitem__(&self, index: i32) -> PyResult<i32> {
-        let i = index as usize;
-        if i < self.elements.len() {
-            Ok(self.elements[i])
-        } else {
-            Err(pyo3::exceptions::PyIndexError::new_err("Index out of range!"))
-        }
-    }
-
-    fn __setitem__(&mut self, index: i32, value: i32) -> PyResult<()> {
-        let i = index as usize;
-        if i < self.elements.len() {
-            self.elements[i] = value;
-            Ok(())
-        } else {
-            Err(pyo3::exceptions::PyIndexError::new_err("Index out of range!"))
-        }
-    }
-
-    fn __delitem__(&mut self, index: i32) -> PyResult<()> {
-        let i = index as usize;
-        if i < self.elements.len() {
-            self.elements.remove(i);
-            Ok(())
-        } else {
-            Err(pyo3::exceptions::PyIndexError::new_err("Index out of range!"))
-        }
-    }
-
-    // fn __iter__(&self) -> PyResult<iter::Iter<i32>> {
-    //     Ok(self.elements.iter().cloned().into_iter())
-    // }
-
-    fn __contains__(&self, element: i32) -> bool {
-        self.elements.contains(&element)
-    }
-
-    fn __str__(&self) -> String {
-        format!("{:?}", self.elements)
+    fn new(value: i32) -> Self {
+        int(Integer::from(value))
     }
 
     fn __repr__(&self) -> String {
-        format!("List({:?})", self.elements)
+        format!("int({})", self.0)
     }
 
-    fn __add__(&self, other: &List) -> List {
-        List {
-            elements: self.elements.iter().chain(other.elements.iter()).cloned().collect()
+    fn __str__(&self) -> String {
+        self.0.to_string()
+    }
+
+    // fn __eq__(&self, other: &Bound<'_, PyAny>) -> PyResult<bool> {
+    //     if let Ok(other_instance) = other.downcast::<Self>() {
+    //         Ok(self.0 == other_instance.borrow().0)
+    //     } else if let Ok(other_int) = other.extract::<BigInt>() {
+    //         Ok(self.0 == BigInt::from(other_int))
+    //     } else {
+    //         Ok(false)
+    //     }
+    // }
+
+    fn __richcmp__(&self, other: &Bound<'_, PyAny>, op: CompareOp) -> PyResult<bool> {
+        match op {
+            CompareOp::Eq => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 == other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 == BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
+            CompareOp::Lt => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 < other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 < BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
+            CompareOp::Le => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 <= other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 <= BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
+            CompareOp::Ne => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 != other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 != BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
+            CompareOp::Ge => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 >= other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 >= BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
+            CompareOp::Gt => {
+                if let Ok(other_instance) = other.downcast::<Self>() {
+                    Ok(self.0 > other_instance.borrow().0)
+                } else if let Ok(other_int) = other.extract::<BigInt>() {
+                    Ok(self.0 > BigInt::from(other_int))
+                } else {
+                    Ok(false)
+                }
+            },
         }
     }
+    
+    // fn __eq__(&self, other: &Bound<'_, PyAny>) -> PyResult<bool> {
+    //     // Attempt to extract a reference to Self from the other object
+    //     if let Ok(other_instance) = other.extract::<Self>() {
+    //         // Compare the internal values for equality
+    //         Ok(self.0 == other_instance.0)
+    //     } else {
+    //         // If extraction fails, the objects are not equal
+    //         Ok(false)
+    //     }
+    // }
 
-    fn __mul__(&self, other: i32) -> List {
-        List {
-            elements: self.elements.iter().cloned().cycle().take(self.elements.len() * other as usize).collect()
-        }
-    }
+    // fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
+    //     op.matches(self.0.cmp(&other.0))
+    // }
 
-
+    // fn __bool__(&self) -> bool {
+    //     self.0 != 0
+    // }
 }
 
-/// A Python module implemented in Rust.
+// #[pyclass]
+// struct num(i32);
+
+// #[pymethods]
+// impl num {
+//     #[new]
+//     fn new(value: i32) -> Self {
+//         Self(value)
+//     }
+
+//     fn __repr__(slf: &Bound<'_, Self>) -> PyResult<String> {
+//         let class_name: Bound<'_, PyString> = slf.get_type().qualname()?;
+//         Ok(format!("{}({})", class_name, slf.borrow().0))
+//     }
+
+//     fn __str__(&self) -> String {
+//         self.0.to_string()
+//     }
+
+//     fn __hash__(&self) -> u64 {
+//         let mut hasher = DefaultHasher::new();
+//         self.0.hash(&mut hasher);
+//         hasher.finish()
+//     }
+
+//     fn __richcmp__(&self, other: &Self, op: CompareOp) -> PyResult<bool> {
+//         match op {
+//             CompareOp::Lt => Ok(self.0 < other.0),
+//             CompareOp::Le => Ok(self.0 <= other.0),
+//             CompareOp::Eq => Ok(self.0 == other.0),
+//             CompareOp::Ne => Ok(self.0 != other.0),
+//             CompareOp::Ge => Ok(self.0 >= other.0),
+//             CompareOp::Gt => Ok(self.0 > other.0),
+//         }
+//     }
+
+//     // fn __richcmp__(&self, other: &Self, op: CompareOp) -> bool {
+//     //     op.matches(self.0.cmp(&other.0))
+//     // }
+
+//     fn __bool__(&self) -> bool {
+//         self.0 != 0
+//     }
+// }
+
+
+// #[derive(FromPyObject)]
+// #[pyclass]
+// struct list {
+//     iterable: Vec<PyObject>,
+// }
+
+
+// #[pymethods]
+// impl list {
+//     #[new]
+//     #[pyo3(signature = (iterable=None))]
+//     fn new(iterable: Option<Vec<PyObject>>) -> Self {
+//         list {
+//             iterable: iterable.unwrap_or_default(),
+//         }
+//     }
+
+    // fn __eq__(&self, other: &Bound<'_, PyAny>) -> PyResult<bool> {
+    //     let a = other.downcast::<list>()?;
+    //     // let b = self.__eq__(a).is_ok();
+    //     let b = self.iterable.iter().eq(other.iterable.iter());
+
+    //     Ok(true)
+        
+    // }
+
+    // fn __eq__(&self, other: PyObject) -> bool {
+    //     Python::with_gil(|py| {
+    //         // Attempt to downcast the other object to a `list`
+    //         if let Ok(list_bound) = other.downcast_bound::<list>(py) {
+    //             // Unbind to get the owned Py<list>
+    //             let list_abc: Py<list> = list_bound.clone().unbind();
+    
+    //             // Extract the Rust `list` instance
+    //             let other_list = list_abc.borrow(py);
+    
+                
+    //             // Compare the `iterable` of both lists
+    //             self.iterable == other_list.iterable
+    //         } else {
+    //             false // Return false if the other object is not a `list`
+    //         }
+    //     })
+    // }
+// }
+            
+            // if let Ok(other) = other.extract::<list>(py) {
+            //     return self.iterable == other.iterable;
+            // }
+
+            // let class_bound = class.downcast_bound::<Class>(py)?;
+
+            // Alternatively you can get a `PyRefMut` directly
+            // let class_ref: PyRefMut<'_, Class> = class.extract(py)?;
+            // assert_eq!(class_ref.i, 1);
+
+//             if other.downcast_bound::<list>(py).is_ok() {
+
+//                 return true;
+//             }
+
+//             if other.downcast_bound::<PyList>(py).is_ok() {
+//                 return true;
+//             }
+
+//             if other.downcast_bound::<PySequence>(py).is_ok() {
+//                 return true;
+//             }
+
+//             false
+//         })
+//     }
+// }
+
+
+// #[pyclass]
+// struct List {
+//     elements: Vec<PyObject>,
+// }
+
+
+
+// #[pymethods]
+// impl List {
+//     #[new]
+//     #[pyo3(signature = (elements=None))]
+//     fn new(py: Python, elements: Option<&Bound<PyAny>>) -> PyResult<Self> {
+//         let mut vec = Vec::new();
+
+//         if let Some(iterable) = elements {
+//             if let Ok(seq) = iterable.downcast::<PySequence>() {
+//                 for item in seq.try_iter()? {
+//                     vec.push(item?.into_pyobject(py)?.unbind());
+//                 }
+//             } else {
+//                 for item in iterable.try_iter()? {
+//                     vec.push(item?.into_pyobject(py)?.unbind());
+//                 }
+//             }
+//         }
+
+//         Ok(List { elements: vec })
+//     }
+// }
+    // /// Appends a new element to the list.
+    // fn append(&mut self, element: PyObject) {
+    //     self.elements.push(element);
+    // }
+
+    // fn __len__(&self) -> usize {
+    //     self.elements.len()
+    // }
+
+
+    // fn __getitem__(&self, index: isize, py: Python) -> PyResult<PyObject> {
+    //     let idx = if index < 0 {
+    //         self.elements.len().checked_sub((-index) as usize)
+    //     } else {
+    //         Some(index as usize)
+    //     }.ok_or_else(|| PyIndexError::new_err("Index out of range"))?;
+
+    //     self.elements.get(idx)
+    //         .cloned()
+    //         .ok_or_else(|| PyIndexError::new_err("Index out of range"))
+    // }
+
+    // fn __setitem__(&mut self, index: isize, value: PyObject) -> PyResult<()> {
+    //     let idx = if index < 0 {
+    //         self.elements.len().checked_sub((-index) as usize)
+    //     } else {
+    //         Some(index as usize)
+    //     }.ok_or_else(|| PyIndexError::new_err("Index out of range"))?;
+
+    //     if idx < self.elements.len() {
+    //         self.elements[idx] = value;
+    //         Ok(())
+    //     } else {
+    //         Err(PyIndexError::new_err("Index out of range"))
+    //     }
+    // }
+
+    // fn __delitem__(&mut self, index: isize) -> PyResult<()> {
+    //     let idx = if index < 0 {
+    //         self.elements.len().checked_sub((-index) as usize)
+    //     } else {
+    //         Some(index as usize)
+    //     }.ok_or_else(|| PyIndexError::new_err("Index out of range"))?;
+
+    //     if idx < self.elements.len() {
+    //         self.elements.remove(idx);
+    //         Ok(())
+    //     } else {
+    //         Err(PyIndexError::new_err("Index out of range"))
+    //     }
+    // }
+
+    // fn __contains__(&self, py: Python, element: PyObject) -> PyResult<bool> {
+    //     for obj in &self.elements {
+    //         if obj.downcast_bound::<PyAny>(py)?.rich_compare(element.downcast_bound::<PyAny>(py)?, CompareOp::Eq)?.is_truthy()? {
+    //             return Ok(true);
+    //         }
+    //     }
+    //     Ok(false)
+    // }
+
+    // fn __str__(&self, py: Python) -> PyResult<String> {
+    //     let mut s = String::from("[");
+    //     for (i, obj) in self.elements.iter().enumerate() {
+    //         if i > 0 {
+    //             s.push_str(", ");
+    //         }
+    //         let obj_ref = obj.as_ref(py); // Obtain a &PyAny reference
+    //         let obj_str = obj_ref.repr()?.to_str()?; // Call repr() and convert to &str
+    //         s.push_str(obj_str);
+    //     }
+    //     s.push(']');
+    //     Ok(s)
+    // }
+    
+
+    // fn __repr__(&self, py: Python) -> PyResult<String> {
+    //     Ok(format!("List({})", self.__str__(py)?))
+    // }
+
+    /// Concatenates two lists.
+    // fn __add__(&self, other: &List) -> List {
+    //     let mut new_elements = self.elements.clone();
+    //     new_elements.extend_from_slice(&other.elements);
+    //     List {
+    //         elements: new_elements,
+    //     }
+    // }
+
+    // fn __mul__(&self, times: isize) -> List {
+    //     let mut new_elements = Vec::new();
+    //     for _ in 0..times.max(0) {
+    //         new_elements.extend(self.elements.iter().cloned());
+    //     }
+    //     List {
+    //         elements: new_elements,
+    //     }
+    // }
+// }
+
+
+
 #[pymodule]
 fn rustique(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_class::<List>()?;
+    m.add_class::<int>()?;
+    // m.add_class::<num>()?;
+    // m.add_class::<list>()?;
     Ok(())
 }
